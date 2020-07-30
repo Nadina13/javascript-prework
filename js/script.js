9fcd0a7bdd226148816c70c1f6dd64eeca7567bc
Nadina Milewska <milewska.nadina@gmail.com>
2020-07-30 17:05:28 +0200

    Add computer and player move

--- js/script.js
+++ js/script.js
@@ -1,3 +1,48 @@
-let playerMove = 'papier'
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Mój ruch to:' + playerMove + '! Wygrywam?');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+if(randomNumber == 2){
+  computerMove = 'papier';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+if(randomNumber == 3){
+  computerMove = 'nożyczki';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if(playerInput == '2'){
+  playerMove = 'papier';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if(playerInput == '3'){
+  playerMove = 'nożyczki';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
